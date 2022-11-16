efa8bfb6704fe791367c61675e76389854e1a51b
azizmohammad <fahimaziz.dev@gmail.com>
2022-11-16 18:02:43 +0600

    tastimonail

--- src/Pages/HomePage/Testimonial/SingleReview.jsx
+++ src/Pages/HomePage/Testimonial/SingleReview.jsx
@@ -8,8 +8,16 @@ const SingleReview = ({ review }) => {
         <div className="card bg-white shadow-xl text-primary-content">
             <div className="card-body">
                 <p>{userReview}</p>
-                <div className="card-actions justify-start mt-3">
-                    <img className='w-14 h-14' src={img} alt="" />
+                <div className="card-actions justify-start mt-4 items-center">
+                    <div className="avatar">
+                        <div className="w-16 h-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img src={img} alt="" />
+                        </div>
+                    </div>
+                    <div>
+                        <h2 className='text-lg text-neutral font-semibold'>{name}</h2>
+                        <h4>{location}</h4>
+                    </div>
                 </div>
             </div>
         </div>
